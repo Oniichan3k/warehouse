c5abbc1a3edee72f238c7ebaffdb5df48073f7ed
Oniichan3k <nguyenduchip1@gmail.com>
2025-07-13 20:36:25 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,22 @@ const withTranspileModules = withTM([
   "antd",
   "@ant-design/icons",
   "@ant-design/icons-svg",
+  "rc-util",
+  "rc-picker",
 ]);
 
 const nextConfig: NextConfig = {
   // Các config khác của bạn ở đây (nếu có)
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Fix for rc-util module resolution
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'rc-util/es/Dom/canUseDom': 'rc-util/lib/Dom/canUseDom',
+    };
+    
+    return config;
+  },
   // ... (thêm config khác nếu cần)
 };
 
